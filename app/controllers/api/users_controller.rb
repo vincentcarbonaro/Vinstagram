0e2c99c622ec1f8fb3bded7036094b1637dcae07
vincentcarbonaro <nd4spd356@gmail.com>
2015-03-13 14:32:40 -0400

    ...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,10 +15,25 @@ module Api
       render :json => {}
     end
 
-    private
+    # private
     # def user_params
     #   params.require(:user).permit(:picture)
     # end
 
+
   end
 end
+
+
+# create_table "users", force: :cascade do |t|
+#   t.string   "username",             null: false
+#   t.string   "email",                null: false
+#   t.string   "password_digest",      null: false
+#   t.string   "session_token"
+#   t.datetime "created_at"
+#   t.datetime "updated_at"
+#   t.string   "picture_file_name"
+#   t.string   "picture_content_type"
+#   t.integer  "picture_file_size"
+#   t.datetime "picture_updated_at"
+# end
