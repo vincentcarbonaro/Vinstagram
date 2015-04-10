9a5d6162a6b81d8d4d3779be6846e8a222646d31
vincentcarbonaro <nd4spd356@gmail.com>
2015-04-10 16:36:56 -0400

    Adding Seed Files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# User.create!(username: "guest",
+#              email: "guest@lol.com",
+#              password: "superpassword",
+#              password_confirmation: "superpassword",
+#              picture: File.open("#{Rails.root}/app/assets/images/default_seeds/guest.jpg")
+#             )
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# User.create!(username: "username",
+#              email: "vin@lol.com",
+#              password: "superpassword",
+#              password_confirmation: "superpassword",
+#              picture: File.open("#{Rails.root}/app/assets/images/default_seeds/vin.jpeg")
+#             )
 
-
-
-
-# create_table "posts", force: :cascade do |t|
-#   t.binary   "img",        null: false
-#   t.integer  "author_id",  null: false
-#   t.string   "text",       null: false
-#   t.datetime "created_at"
-#   t.datetime "updated_at"
-# end
-#
-# create_table "users", force: :cascade do |t|
-#   t.string   "username",        null: false
-#   t.string   "email",           null: false
-#   t.string   "password_digest", null: false
-#   t.string   "session_token"
-#   t.datetime "created_at"
-#   t.datetime "updated_at"
-# end
+200.times do
+  numb = Random.rand(22) + 1
+  User.create!(username: (0...10).map { ('a'..'z').to_a[rand(26)] }.join,
+              email: (0...10).map { ('a'..'z').to_a[rand(26)] }.join + "@lol.com",
+              password: "password",
+              password_confirmation: "password",
+              picture: File.open("#{Rails.root}/app/assets/images/default_seeds/aa#{numb}.jpg"))
+end
