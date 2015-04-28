64da49612a26508a0e2f0dbd41d568ef87c7065d
vincentcarbonaro <nd4spd356@gmail.com>
2015-04-28 00:01:05 -0400

    Removed finding by provider

--- app/controllers/googles_controller.rb
+++ app/controllers/googles_controller.rb
@@ -2,7 +2,8 @@ class GooglesController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.find_by_uid(auth["uid"]) || User.create_with_omniauth(auth)
+    # user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     sign_in(user)
     redirect_to root_url
   end
